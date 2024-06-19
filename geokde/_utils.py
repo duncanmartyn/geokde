fix: amended array bounds calculation to mitigate minor resolution loss/gain

geokde/_utils.py
@@ -13,9 +13,9 @@ from geokde._kernels import (
 
 
 def validate_transform(
-        arg: int | float | str,
-        arg_name: str,
-        points: gpd.GeoDataFrame | gpd.GeoSeries,
+    arg: int | float | str,
+    arg_name: str,
+    points: gpd.GeoDataFrame | gpd.GeoSeries,
 ) -> np.ndarray:
     """Validation and transformation function for weight and radius arguments.
 
@@ -62,66 +62,20 @@ def validate_transform(
     return arg
 
 
-def adjust_bounds(
-        minx: int | float,
-        miny: int | float,
-        maxx: int | float,
-        maxy: int | float,
-        radius: int | float,
-) -> list[int | float]:
-    """Calculate radius-adjusted bounds for the KDE array.
+def create_array(
+    bounds: np.ndarray,
+    radius: int | float,
+    resolution: int | float,
+) -> tuple[np.ndarray, list[int | float]]:
+    """Generate an ndarray of shape derived from bounding coordinates and spatial
+    resolution and calculate the radius- and shape-adjusted bounds thereof.
 
     Parameters
     ----------
-    minx : int | float
-        Minimum X coordinate to adjust.
-    miny : int | float
-        Minimum Y coordinate to adjust.
-    maxx : int | float
-        Maximum X coordinate to adjust.
-    maxy : int | float
-        Maximum Y coordinate to adjust.
+    bounds : numpy.ndarray
+        Bounding coordinates to adjust.
     radius : int | float
         Radius with which to adjust coordinates.
-
-    Returns
-    -------
-    minx : int | float
-        Radius-adjusted minimum X coordinate.
-    miny : int | float
-        Radius-adjusted minimum Y coordinate.
-    maxx : int | float
-        Radius-adjusted maximum X coordinate.
-    maxy : int | float
-        Radius-adjusted maximum Y coordinate.
-    """
-    minx -= radius
-    miny -= radius
-    maxx += radius
-    maxy += radius
-    return [minx, miny, maxx, maxy]
-
-
-def create_array(
-        minx: int | float,
-        miny: int | float,
-        maxx: int | float,
-        maxy: int | float,
-        resolution: int | float,
-) -> np.ndarray:
-    """Generate an ndarray of shape derived from bounding coordinates and spatial
-    resolution.
-
-    Parameters
-    ----------
-    minx : int | float
-        Minimum X coordinate for the output array.
-    miny : int | float
-        Minimum Y coordinate for the output array.
-    maxx : int | float
-        Maximum X coordinate for the output array.
-    maxy : int | float
-        Maximum Y coordinate for the output array.
     resolution : int | float
         Spatial resolution for the output array.
 
@@ -129,20 +83,27 @@ def create_array(
     -------
     array : numpy.ndarray
         Array of shape determined using bounding coordinates and resolution.
+    list[int | float]
+        List of minx, miny, maxx, and maxy bounding coordinates for the array.
     """
-    width = round((maxx - minx) / resolution)
-    height = round((maxy - miny) / resolution)
+    minx, miny, maxx, maxy = bounds
+    minx -= radius
+    maxy += radius
+    width = round((maxx + radius - minx) / resolution)
+    height = round((maxy - (miny - radius)) / resolution)
+    maxx = minx + width * resolution
+    miny = maxy - height * resolution
     array = np.full((height, width), 0.0)
-    return array
+    return array, [minx, miny, maxx, maxy]
 
 
 def get_points(
-        points: gpd.GeoDataFrame | gpd.GeoSeries,
-        minx: int | float,
-        maxy: int | float,
-        radius: np.ndarray,
-        weight: np.ndarray,
-        resolution: int | float,
+    points: gpd.GeoDataFrame | gpd.GeoSeries,
+    minx: int | float,
+    maxy: int | float,
+    radius: np.ndarray,
+    weight: np.ndarray,
+    resolution: int | float,
 ) -> np.ndarray:
     """Generate an ndarray of point X and Y array coordinates, search window radii, and
     weights.
@@ -176,10 +137,10 @@ def get_points(
 
 
 def calculate_kde(
-        points: np.ndarray,
-        array: np.ndarray,
-        kernel: str,
-        scale: bool,
+    points: np.ndarray,
+    array: np.ndarray,
+    kernel: str,
+    scale: bool,
 ) -> None:
     """Iterate over elements of an ndarray comprising point properties.
 
@@ -202,39 +163,32 @@ def calculate_kde(
     }
     # challenge to vectorise as needs to operate on >1 array element
     for point in points:
-        add_point_kde(*point, array, kernel_vfuncs[kernel])
+        add_point_kde(point, array, kernel_vfuncs[kernel])
 
 
 def add_point_kde(
-        x: int | float,
-        y: int | float,
-        window: float,
-        weight: int | float,
-        array: np.ndarray,
-        kernel_vfunc: np.vectorize,
+    point: np.ndarray,
+    array: np.ndarray,
+    kernel_vfunc: np.vectorize,
 ) -> None:
-    """Perform KDE for a given point, window, and weight, adding the result to an array.
+    """Perform KDE for a given point, adding the result to an array.
 
     Parameters
     ----------
-    x : int | float
-        Array X coordinate for a point.
-    y : int | float
-        Array Y coordinate for a point.
-    window : int | float
-        Search window radius within which to perform KDE for a point.
-    weight: int | float
-        Value with which the KDE value for a point will be weighted.
+    point : numpy.ndarray
+        Properties of the point for which KDE will be calculated, including array X and
+        Y coordinates, search window radius, and weight.
     array : numpy.ndarray
         Array to which KDE values will be added.
     kernel_vfunc : numpy.vectorize
         Vectorised kernel function with which to perform KDE.
     """
+    x, y, window, weight = point
     minx = round(x - window)
     miny = round(y - window)
     maxx = round(x + window)
     maxy = round(y + window)
-    y_idx, x_idx = np.ogrid[miny + .5:maxy + .5, minx + .5:maxx + .5]
+    y_idx, x_idx = np.ogrid[miny + 0.5 : maxy + 0.5, minx + 0.5 : maxx + 0.5]
     dist_array = np.sqrt(pow(x_idx - x, 2) + pow(y_idx - y, 2))
     kde_array = kernel_vfunc(dist_array, window, weight)
     array[miny:maxy, minx:maxx] += kde_array

geokde/geokde.py
@@ -3,7 +3,6 @@ import numpy as np
 
 from geokde._kernels import VALID_KERNELS
 from geokde._utils import (
-    adjust_bounds,
     calculate_kde,
     create_array,
     get_points,
@@ -12,12 +11,12 @@ from geokde._utils import (
 
 
 def kde(
-        points: gpd.GeoDataFrame | gpd.GeoSeries,
-        radius: int | float | str,
-        resolution: int | float,
-        kernel: str = "quartic",
-        weight: int | float | str = 1.0,
-        scale: bool = False,
+    points: gpd.GeoDataFrame | gpd.GeoSeries,
+    radius: int | float | str,
+    resolution: int | float,
+    kernel: str = "quartic",
+    weight: int | float | str = 1.0,
+    scale: bool = False,
 ) -> tuple[np.ndarray, list[int | float]]:
     """Estimate raw or scaled kernel density with a given radius (or radii), resolution,
     kernel, and weight(s) from point geometries in a given GeoDataFrame or GeoSeries.
@@ -91,13 +90,19 @@ def kde(
         raise ValueError(f"kernel must be one of {VALID_KERNELS}, not: {kernel}")
     if not isinstance(scale, bool):
         raise TypeError(f"scale must be bool, not: {type(scale)}")
-    radius = validate_transform(radius, "radius", points)
-    weight = validate_transform(weight, "weight", points)
-    if resolution > radius.max():
+    radius_arr = validate_transform(radius, "radius", points)
+    weight_arr = validate_transform(weight, "weight", points)
+    if resolution > radius_arr.max():
         raise ValueError("resolution must be less than radius.")
 
-    bounds = adjust_bounds(*points.total_bounds, radius.max())
-    array = create_array(*bounds, resolution)
-    points = get_points(points, bounds[0], bounds[3], radius, weight, resolution)
+    array, bounds = create_array(points.total_bounds, radius_arr.max(), resolution)
+    points = get_points(
+        points,
+        bounds[0],
+        bounds[3],
+        radius_arr,
+        weight_arr,
+        resolution,
+    )
     calculate_kde(points, array, kernel, scale)
     return array, bounds

tests/test_geokde.py
@@ -5,24 +5,24 @@ from geokde.geokde import kde
 
 
 @pytest.mark.parametrize(
-        ["geom_type", "radius", "resolution", "kernel", "weight", "scale", "error"],
-        [
-            # all valid
-            ("points", 1, 0.1, "quartic", 1, False, None),
-            # invalid geoms
-            ("polygons", 1, 0.1, "quartic", 1, False, TypeError),
-            # invalid kernel
-            ("points", 1, 0.1, "invalid_kernel", 1, False, ValueError),
-            # invalid scale
-            ("points", 1, 0.1, "quartic", 1, "invalid_scale", TypeError),
-            # all valid, gdf column "radius" as KDE radii
-            ("points", "radius", 0.1, "quartic", 1, False, None),
-            # gdf column of type object as radii
-            ("points", "radius", 0.1, "quartic", 1, False, TypeError),
-            # incomplete gdf column as radii
-            ("points", "radius", 0.1, "quartic", 1, False, ValueError),
-            # needs test for max radius lt resolution
-        ],
+    ["geom_type", "radius", "resolution", "kernel", "weight", "scale", "error"],
+    [
+        # all valid
+        ("points", 1, 0.1, "quartic", 1, False, None),
+        # invalid geoms
+        ("polygons", 1, 0.1, "quartic", 1, False, TypeError),
+        # invalid kernel
+        ("points", 1, 0.1, "invalid_kernel", 1, False, ValueError),
+        # invalid scale
+        ("points", 1, 0.1, "quartic", 1, "invalid_scale", TypeError),
+        # all valid, gdf column "radius" as KDE radii
+        ("points", "radius", 0.1, "quartic", 1, False, None),
+        # gdf column of type object as radii
+        ("points", "radius", 0.1, "quartic", 1, False, TypeError),
+        # incomplete gdf column as radii
+        ("points", "radius", 0.1, "quartic", 1, False, ValueError),
+        # needs test for max radius lt resolution
+    ],
 )
 def test_kde(geodataframe, geom_type, radius, resolution, kernel, weight, scale, error):
     gdf = geodataframe(geom_type)
@@ -35,6 +35,6 @@ def test_kde(geodataframe, geom_type, radius, resolution, kernel, weight, scale,
             kde(gdf, radius, resolution, kernel, weight, scale)
     else:
         array, bounds = kde(gdf, radius, resolution, kernel, weight, scale)
-        assert isinstance(array, np.ndarray)  # nosec
-        assert isinstance(bounds, list)  # nosec
+        assert isinstance(array, np.ndarray)
+        assert isinstance(bounds, list)
         # TODO: more detailed assertions

tests/test_utils.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 from geokde._utils import (
-    adjust_bounds,
     create_array,
     validate_transform,
 )
@@ -10,22 +9,18 @@ from geokde._utils import (
 def test_validate_transform(geodataframe):
     points = geodataframe("points")
     result = validate_transform("radius", "radius", points)
-    assert isinstance(result, np.ndarray)  # nosec
-    assert len(result) == len(points)  # nosec
-
-
-def test_adjust_bounds():
-    radius = 10
-    bounds = [-10, -10, 10, 10]
-    expected = [-20, -20, 20, 20]
-    result = adjust_bounds(*bounds, radius)
-    assert result == expected  # nosec
+    assert isinstance(result, np.ndarray)
+    assert len(result) == len(points)
 
 
 def test_create_array():
+    radius = 10
     resolution = 1
-    bounds = [-10, -10, 10, 10]
-    array = create_array(*bounds, resolution)
-    assert array.shape == (20, 20)  # nosec
+    bounds = [-10.0, -10.0, 10.0, 10.0]
+    expected = [-20, -20, 20, 20]
+    array, bounds = create_array(bounds, radius, resolution)
+    assert array.shape == (40, 40)
+    assert bounds == expected
+
 
 # TODO: final three _utils fn tests, though all covered by test_geokde